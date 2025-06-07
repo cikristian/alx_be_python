308502b55b9f5508d057467c675e17229d5f830a
cikristian <rwibutsohakizinkachristian@gmail.com>
2025-06-07 22:44:43 +0200

    function operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,19 +1,18 @@
-from arithmetic_operations import perform_operation
 
 def perform_operation():
    num1 = float(input("Enter first number: "))
    num2 = float(input("Enter second number: "))
    operation = input("Enter operation (+, -, *, /): ") 
-    if operation == "+":
+   if operation == "+":
          result = num1 + num2
-    elif operation == "-":
+   elif operation == "-":
          result = num1 - num2
-    elif operation == "*":
+   elif operation == "*":
          result = num1 * num2
-    elif operation == "/":
+   elif operation == "/":
          if num2 != 0:
               result = num1 / num2
          else:
               return "Error: Division by zero is not allowed."
-    else:
+   else:
          return "Error: Invalid operation."
