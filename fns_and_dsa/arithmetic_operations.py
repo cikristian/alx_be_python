a42884190f69d473d1411bcb46caf680c8675ac4
cikristian <rwibutsohakizinkachristian@gmail.com>
2025-06-08 11:53:41 +0200

    operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,7 +7,7 @@ def perform_operation(num1, num2, operation):
          result = num1 + num2
    elif operation == "subtract":
          result = num1 - num2
-   elif operation == "mulitiply":
+   elif operation == "multiply":
          result = num1 * num2
    elif operation == "divide":
          if num2 != 0:
