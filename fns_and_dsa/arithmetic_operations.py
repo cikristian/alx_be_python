2e928ea9140ae634b184c7cabe3206a0101329a2
cikristian <rwibutsohakizinkachristian@gmail.com>
2025-06-08 11:52:50 +0200

    operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -3,13 +3,13 @@ def perform_operation(num1, num2, operation):
    num1 = float(input("Enter first number: "))
    num2 = float(input("Enter second number: "))
    operation = input("Enter operation (+, -, *, /): ") 
-   if operation == "+":
+   if operation == "add":
          result = num1 + num2
-   elif operation == "-":
+   elif operation == "subtract":
          result = num1 - num2
-   elif operation == "*":
+   elif operation == "mulitiply":
          result = num1 * num2
-   elif operation == "/":
+   elif operation == "divide":
          if num2 != 0:
               result = num1 / num2
          else:
